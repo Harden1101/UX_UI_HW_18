88be4ce7d20747a6e2255da43080cbaed55af419
Harden1101 <139825969+Harden1101@users.noreply.github.com>
2023-12-16 19:26:29 -0600

    links redone on nav

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+
+Trying to setup Work cards rearange. this works but
+I can not remove the hover state since its pseudoclasses?
+
+const CardSep = document.querySelector('.bck_img_cards');
+
+CardSep.addEventListener('click', () => {
+    CardSep.classList.add('bck_img_cards_active');
+    
+
+})
+
+*/
+
+
+
+
+
+
