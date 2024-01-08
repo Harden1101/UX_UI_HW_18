dbcdec6e779dee061b87aac37ec208f7bcbbb8c2
Harden1101 <139825969+Harden1101@users.noreply.github.com>
2024-01-08 16:06:26 -0600

    sasd

--- script.js
+++ script.js
@@ -17,6 +17,29 @@ $(".navbar-nav .nav-link").on('click', function () {
     $(".navbar-toggler").click(); // Simulate a click on the navbar-toggler button
   });
 
+  document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', function (event) {
+        const requiredFields = document.querySelectorAll('.required-field');
+
+        if (form.getAttribute('data-submitted') === 'true') {
+            requiredFields.forEach(function (field) {
+                if (!field.checkValidity()) {
+                    field.classList.add('invalid');
+                    event.preventDefault();
+                } else {
+                    field.classList.remove('invalid');
+                }
+            });
+        } else {
+            // Set the data-submitted attribute to true when the form is submitted for the first time
+            form.setAttribute('data-submitted', 'true');
+        }
+    });
+});
+
+
 
 
 
