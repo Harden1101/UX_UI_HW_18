51491e8292ae25d53064227beed5a6f9a66952d4
Harden1101 <139825969+Harden1101@users.noreply.github.com>
2024-01-10 14:11:51 -0600

    Prototype now linked
    depending on screen size different version of the prototype will work (desk top or mobile)

--- script.js
+++ script.js
@@ -40,6 +40,25 @@ $(".navbar-nav .nav-link").on('click', function () {
 });
 
 
+// changing my bck link for mobile or desktop protp //
+
+function updateLinkDestination() {
+    var link = document.getElementById('BCK_proto_site');
+
+    if (window.innerWidth < 768) {
+        // Change the destination for screens below 768px
+        link.href = 'https://www.figma.com/proto/dT5NzUkXYuWhPhJ87lGACZ/BCK-Hi-Fi-mobile?type=design&node-id=377-4475&t=YessHETsnQuUvH9F-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=377%3A4475&mode=design';
+    } else {
+        // Keep the original destination for screens wider than 768px
+        link.href = 'https://www.figma.com/proto/FHJuD7GEA3pkUOiK6u7A49/BCK-Hi-fi-Desktop?type=design&node-id=1793-53790&t=as3dO6d2D8bnFQJL-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1793%3A53790&mode=design';
+    }
+}
+
+// Initial call to set the link destination based on the initial screen width
+updateLinkDestination();
+
+// Listen for window resize events to update the link destination dynamically
+window.addEventListener('resize', updateLinkDestination);
 
 
 
